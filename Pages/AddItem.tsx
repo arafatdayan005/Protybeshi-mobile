1a15238a760c4b12546d46cb166398da73e78aaf
arafatdayan005 <arafatdayan005@gmail.com>
2023-12-09 02:50:39 +0600

    Adding Items to DB Added

--- Pages/AddItem.tsx
+++ Pages/AddItem.tsx
@@ -1,43 +1,86 @@
-import { View, Text, TextInput, TouchableOpacity } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import React, { useState } from "react";
+import axios from "axios";
 
 const AddItem = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [safetyMoney, setSafetyMoney] = useState("");
+  const [image, setImage] = useState("");
+  const [contact, setContact] = useState("");
+
+  const handlePost = () => {
+    const itemData = {
+      name: name,
+      description: description,
+      safetyMoney: parseFloat(safetyMoney),
+      img: image,
+      contact: parseInt(contact),
+    };
+
+    axios.post("http://192.168.0.106:3000/items", itemData).then(() => {
+      setName("");
+      setDescription("");
+      setSafetyMoney("");
+      setImage("");
+      setContact("");
+    });
+  };
+
   return (
-    <View className="w-full flex bg-[#F6FFFB] h-full pt-12">
-      <Text className="text-2xl text-center font-bold">Item Description</Text>
-      <View className="px-6 mt-4">
-        <Text className="text-xl pl-2 mb-2">Name</Text>
-        <TextInput
-          className="border border-gray-300 h-12 rounded-lg w-full pl-4 mb-4 bg-white"
-          placeholder="e.g: Toolbox"
-        />
-        <Text className="text-xl pl-2 mb-2">Description</Text>
-        <TextInput
-          className="border border-gray-300 h-12 rounded-lg w-full pl-4 mb-4 bg-white"
-          placeholder="Item Description"
-          multiline
-          numberOfLines={5}
-        />
-        <Text className="text-xl pl-2 mb-2">Safety Money</Text>
-        <TextInput
-          className="border border-gray-300 h-12 rounded-lg w-full pl-4 mb-4 bg-white"
-          placeholder="e.g: $80"
-        />
-        <Text className="text-xl pl-2 mb-2">Image</Text>
-        <TextInput
-          className="border border-gray-300 h-12 rounded-lg w-full pl-4 mb-4 bg-white"
-          placeholder="Open Gallary"
-        />
-        <Text className="text-xl pl-2 mb-2">Contact</Text>
-        <TextInput
-          className="border border-gray-300 h-12 rounded-lg w-full pl-4 mb-4 bg-white"
-          placeholder="e.g: 0111111111"
-        />
-        <TouchableOpacity className="bg-cyan-400 w-24 px-4 py-3 rounded-full mt-4 mx-auto">
-          <Text className="font-bold text-center text-xl text-white">Post</Text>
-        </TouchableOpacity>
+    <ScrollView>
+      <View className="w-full flex bg-[#F6FFFB] h-full pt-12">
+        <Text className="text-2xl text-center font-bold">Item Description</Text>
+        <View className="px-6 mt-4">
+          <Text className="text-xl pl-2 mb-2">Name</Text>
+          <TextInput
+            className="border border-gray-300 h-12 rounded-lg w-full pl-4 mb-4 bg-white"
+            placeholder="e.g: Toolbox"
+            onChangeText={(text) => setName(text)}
+          />
+          <Text className="text-xl pl-2 mb-2">Description</Text>
+          <TextInput
+            className="border border-gray-300 h-12 rounded-lg w-full pl-4 mb-4 bg-white"
+            placeholder="Item Description"
+            multiline
+            numberOfLines={5}
+            onChangeText={(text) => setDescription(text)}
+          />
+          <Text className="text-xl pl-2 mb-2">Safety Money</Text>
+          <TextInput
+            className="border border-gray-300 h-12 rounded-lg w-full pl-4 mb-4 bg-white"
+            placeholder="e.g: $80"
+            onChangeText={(text) => setSafetyMoney(text)}
+          />
+          <Text className="text-xl pl-2 mb-2">Image</Text>
+          <TextInput
+            className="border border-gray-300 h-12 rounded-lg w-full pl-4 mb-4 bg-white"
+            placeholder="Open Gallary"
+            onChangeText={(text) => setImage(text)}
+          />
+          <Text className="text-xl pl-2 mb-2">Contact</Text>
+          <TextInput
+            className="border border-gray-300 h-12 rounded-lg w-full pl-4 mb-4 bg-white"
+            placeholder="e.g: 0111111111"
+            onChangeText={(text) => setContact(text)}
+          />
+          <TouchableOpacity className="bg-cyan-400 w-24 px-4 py-3 rounded-full my-6 mx-auto">
+            <Text
+              className="font-bold text-center text-xl text-white"
+              onPress={handlePost}
+            >
+              Post
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </ScrollView>
   );
 };
 
--- Pages/Home.tsx
+++ Pages/Home.tsx
@@ -1,10 +1,18 @@
-import { View, Text, Image, TextInput, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  TextInput,
+  ScrollView,
+  RefreshControl,
+} from "react-native";
 import React, { useEffect, useState } from "react";
 import ItemCards from "../Components/ItemCards";
 import axios from "axios";
 
 const Home = () => {
   const [data, setData] = useState([]);
+  const [refreshing, setRefreshing] = useState(false);
 
   useEffect(() => {
     fetchData();
@@ -12,15 +20,29 @@ const Home = () => {
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://192.168.0.106:3000/items');
-      setData(response.data)
+      const response = await axios.get("http://192.168.0.106:3000/items");
+      setData(response.data);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
     }
   };
 
+  const onRefresh = () => {
+    setRefreshing(true);
+    fetchData();
+    setRefreshing(false);
+  };
+
   return (
-    <ScrollView>
+    <ScrollView
+      refreshControl={
+        <RefreshControl
+          refreshing={refreshing}
+          onRefresh={onRefresh}
+          colors={["#00ffff"]}
+        />
+      }
+    >
       <View className="w-full flex items-center bg-[#F6FFFB] h-full">
         <Text className="text-cyan-500 text-2xl mt-12 text-center">
           Protybeshi
@@ -29,10 +51,12 @@ const Home = () => {
           className="border border-gray-300 h-12 w-80 rounded-full pl-4 mb-4 bg-white mt-2"
           placeholder="Search Item"
         />
-        <View className="mt-4 px-2 flex justify-center flex-row flex-wrap">
-          {
-            data.map((item : object) => <ItemCards key={item._id} item={item} />)
-          }          
+        <View className="mt-4 ml-10">
+          <View className="flex justify-left flex-row flex-wrap">
+            {data.map((item: object) => (
+              <ItemCards key={item._id} item={item} />
+            ))}
+          </View>
         </View>
       </View>
     </ScrollView>
